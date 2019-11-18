e08c7344bc8836a1ef1d61acb5a1f24359d1c249
RWEngelbrecht <rigardt@brechtburg.co.za>
2019-11-18 15:38:57 +0200

    conceptualizing the matching process. what am i missing?

---
+++ controllers/umatch.js
@@ -0,0 +1,11 @@
+const User = require('../models/umod');
+var sessionData;
+
+exports.getMatches = (req, res, next) => {
+	// find all from users where:
+	//	-	match.gender == user.genderpref && match.genderpref == user.gender
+	//	-	user.agepreflower < match.age < user.ageprefupper &&
+	//			match.agepreflower < user.age < match.ageprefupper
+	//	-	at least 1 user.interests[] in match.interests[]
+	//	-	match.location within range of user.location + maxdist
+}
