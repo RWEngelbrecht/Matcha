a778159e37aab8a7341afbef38b916afdef38fcf
rengelbr <rengelbr@student.wethinkcode.co.za>
2019-11-19 06:55:42 +0200

    finds users based on signed in user genderpref. how does swig accept variables?

--- controllers/umatch.js
+++ controllers/umatch.js
@@ -10,9 +10,34 @@ exports.getMatches = (req, res, next) => {
 	//	-	at least 1 user.interests[] in match.interests[]
 	//	-	match.location within range of user.location + maxdist
 	//	-	order by fame desc
+	let message = req.flash('Something went wrong, please try again later!');
+	if (message.length > 0) {
+		message = message[0];
+	} else {
+		message = null;
+	}
+
 	currUser = req.session.user;
+
 	// should not be able to access matches page without being logged in, so no user authentication needed?
-	
-	return (res.render(path.resolve('views/matches')));
+
+	// User.aggregate().group({
+	// 	"matchList": {
+	// 		$addToSet: {
+	// 			gender: currUser.genderpref
+	// 		}
+	// 	}
+	// })
+
+	User.find({gender: currUser.genderpref}, (err, matches) => {
+		if (!matches)
+			console.log('No matches for you!');
+		else
+			matches.forEach(element => {
+				console.log(element.username);
+			});
+	});
+
+	return (res.render(path.resolve('views/matches'), {matches: matches}));
 }
 
