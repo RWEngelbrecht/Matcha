40f1cdfb54ca545436b0df010108c0d16ff27b5a
Arata Yano <aratayano00@gmail.com>
2019-12-03 09:27:35 +0200

    hack fix but location updating in session now at least

--- controllers/user_info.js
+++ controllers/user_info.js
@@ -16,7 +16,8 @@ exports.updateinfo = (req, res, next) => {
 		message = null;
 	}
 	loggedUser = req.session.user.username
-	// console.log(req.session.user);
+	console.log("USER ON DIFFERENT PAGE");
+	console.log(req.session.user);
 	return (res.render(path.resolve('views/update_info'),{
 		user: loggedUser
 	}));
