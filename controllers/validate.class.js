04357ac4da2e0cc313ea7364dafc8dc080cd76a5
Arata Yano <aratayano00@gmail.com>
2019-12-03 11:46:05 +0200

    our own validation working

--- controllers/validate.class.js
+++ controllers/validate.class.js
@@ -6,17 +6,21 @@ class Validate {
 
     ValidatePassword(passwd) {
         // BOOL -> PROMISES? DOESNT SEEM NECESSARY.
-        // ARE WE ALLOWING SPECIAL CHARS?.
+        
         if (passwd.length < 8) {
             console.log("Password must be at least 8 characters long");
             return(0);
         }
+        var checkspecial = new RegExp("\\W");
         var checklower = new RegExp("[a-z]");
         var checkupper = new RegExp("[A-Z]");
         var checknumber = new RegExp("[0-9]");
-        if (checklower.test(passwd) == false || checkupper.test(passwd) == false || checknumber.test(passwd) == false ) {
-            console.log("Password must conatin at least one Uppercase letter and a number");
-            return (0);
+        if (checklower.test(passwd) == false
+            || checkupper.test(passwd) == false
+            || checknumber.test(passwd) == false
+            || checkspecial.test(passwd) == true) {
+                console.log("Password must conatin at least one Uppercase letter and a number and no special characters");
+                return (0);
         }
         return(1);
     };
