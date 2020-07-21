d1af803bbb1c6d7981d5af4bc8c7db3af4bd5824
RWEngelbrecht <rigardt@brechtburg.co.za>
2020-07-21 18:03:13 +0200

    added dotenv - better way to configure server setup

--- app.js
+++ app.js
@@ -6,10 +6,14 @@ const mongoose	= require('mongoose');
 const swig		= require('swig');
 const bodyParser= require('body-parser');
 const flash		= require('connect-flash');
+const dotenv	= require('dotenv');
 MONGODB_URI		= "mongodb+srv://Yano:80058024@cluster0-jszpy.mongodb.net/matcha";
 // MONGODB_URI		= "mongodb+srv://Rigardt:80058024@cluster0-e6mik.mongodb.net/matcha";
 
 // APP SETUP.
+dotenv.config({path: 'conf.env'});
+const pool		= require('./database');
+
 const app = express();
 const server = http.createServer(app);
 var connectedUsers = [];
@@ -55,18 +59,21 @@ app.use(function(err, req, res, next) {
 	res.render('error');
 });
 
-mongoose.set('useFindAndModify', false);
-mongoose.set('useCreateIndex', true);
-mongoose
-.connect(MONGODB_URI, {useNewUrlParser: true, useUnifiedTopology: true})
-.then(result => {
-	server.listen(8000);
-})
-.then(result => {
-	console.log(`Server has started, Port: 8000`);
-})
-.catch(err => {
-	console.log(err)
-});
+// mongoose.set('useFindAndModify', false);
+// mongoose.set('useCreateIndex', true);
+// mongoose
+// .connect(MONGODB_URI, {useNewUrlParser: true, useUnifiedTopology: true})
+// .then(result => {
+// 	server.listen(8000);
+// })
+// .then(result => {
+// 	console.log(`Server has started, Port: 8000`);
+// })
+// .catch(err => {
+// 	console.log(err)
+// });
+
+
+server.listen(process.env.PORT)
 
 require('./routes/sockethandle')(connectedUsers);
